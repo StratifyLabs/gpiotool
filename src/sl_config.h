caf6dbe0c57591742c972637da34ed35a1160b86
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2019-02-05 22:06:12 -0700

    published first build

--- src/sl_config.h
+++ src/sl_config.h
@@ -6,9 +6,9 @@
 #ifndef SL_CONFIG_H_
 #define SL_CONFIG_H_
 
-#define SL_CONFIG_VERSION_STRING "1.0"
-#define SL_CONFIG_VERSION_BCD 0x100
-#define SL_CONFIG_DOCUMENT_ID "<invalid>"
+#define SL_CONFIG_VERSION_STRING "0.1"
+#define SL_CONFIG_VERSION_BCD 0x01
+#define SL_CONFIG_DOCUMENT_ID "PNXzFMNGs0hIWyhMqFsL"
 #define SL_CONFIG_NAME "gpiotool"
 #define SL_CONFIG_TYPE "app"
 #define SL_CONFIG_PUBLISHER "Stratify Labs, Inc"
